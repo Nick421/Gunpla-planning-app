9391c9a8d6eb584fe4fdd397ad838faaa5aa73ec
Niravit Theng <nthe0838@uni.sydney.edu.au>
2021-05-19 00:16:01 +1000

    Update to have basic list, can add, delete and clear project

--- app/src/main/java/com/ntheng/gunpla_planning_app/MainActivity.kt
+++ app/src/main/java/com/ntheng/gunpla_planning_app/MainActivity.kt
@@ -1,27 +1,65 @@
 package com.ntheng.gunpla_planning_app
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
-
-const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    /** Called when the user taps the Send button */
-    fun sendMessage(view: View) {
-        // Do something in response to button
-        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
-        val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+        // Initializing the array lists and the adapter
+        val itemlist = arrayListOf<String>()
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemlist)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val listView = findViewById<ListView>(R.id.listView)
+        // Adding the items to the list when the add button is pressed
+        val add = findViewById<Button>(R.id.add);
+        add.setOnClickListener {
+            itemlist.add(editText.text.toString())
+            listView.adapter =  adapter
+            adapter.notifyDataSetChanged()
+            // This is because every time when you add the item the input      space or the eidt text space will be cleared
+            editText.text.clear()
         }
-        startActivity(intent)
+
+        // Selecting and Deleting the items from the list when the delete button is pressed
+        val delete = findViewById<Button>(R.id.delete)
+        delete.setOnClickListener {
+            val position: SparseBooleanArray = listView.checkedItemPositions
+            val count = listView.count
+            var item = count - 1
+            while (item >= 0) {
+                if (position.get(item))
+                {
+                    adapter.remove(itemlist.get(item))
+                }
+                item--
+            }
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        // Clearing all the items in the list when the clear button is pressed
+        val clear = findViewById<Button>(R.id.clear)
+        clear.setOnClickListener {
+
+            itemlist.clear()
+            adapter.notifyDataSetChanged()
+
+        }
+
+        // Adding the toast message to the list when an item on the list is pressed
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            android.widget.Toast.makeText(this, "You Selected the item --> "+itemlist.get(i), android.widget.Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
